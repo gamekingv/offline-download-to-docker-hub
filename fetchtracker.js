48928dd731ad8194b50ceebd2e7cb1ce17064c9a
gamekingv <793470218@qq.com>
2021-04-09 20:12:46 +0800

    Initialize

--- fetchtracker.js
+++ fetchtracker.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 
 (async () => {
   try {
-    // const { data } = await axios.get('https://ngosang.github.io/trackerslist/trackers_best.txt', { timeout: 10000 });
-    const { data } = await axios.get('https://cdn.jsdelivr.net/gh/ngosang/trackerslist/trackers_best.txt', { timeout: 10000 });
+    const { data } = await axios.get('https://ngosang.github.io/trackerslist/trackers_best.txt', { timeout: 10000 });
+    // const { data } = await axios.get('https://cdn.jsdelivr.net/gh/ngosang/trackerslist/trackers_best.txt', { timeout: 10000 });
     fs.appendFile('aria2.conf',
       `\nbt-tracker=${data.split('\n').filter(s => s && s.trim()).join(',')}\n`,
       'utf-8',
