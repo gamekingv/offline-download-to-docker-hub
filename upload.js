1708d67913ce7dcc2421b752bd40a2f43f33f49f
gamekingv <31853855+gamekingv@users.noreply.github.com>
2021-04-22 17:27:23 +0800

    Update upload.js

--- upload.js
+++ upload.js
@@ -27,8 +27,11 @@ const preset = {
 
 async function errorHandler(error) {
   const config = error.config;
-  if (!config || [401, 404, 504].some(status => error.response.status === status)) return await Promise.reject(error);
-  console.log('请求出错，HTTP状态码：' + error.response.status);
+  if (error.response) {
+    if (!config || [401, 404, 504].some(status => error.response.status === status)) return await Promise.reject(error);
+    console.log('请求出错，HTTP状态码：' + error.response.status);
+  }
+  else console.log(`请求出错：${error.toString()}`);
   config.__retryCount = config.__retryCount || 0;
   if (config.__retryCount >= preset.retry) return await Promise.reject(error);
   config.__retryCount += 1;
