646253c4bfdc7cdde89304a2d732fd35966583e2
gamekingv <793470218@qq.com>
2021-05-25 23:52:57 +0800

    Initialize

--- watch.js
+++ watch.js
@@ -1,18 +1,17 @@
 const got = require('got');
 
-const client = got.extend({
+let client = got.extend({
   timeout: 5000,
   responseType: 'json',
   hooks: {
     afterResponse: [(response, retryWithMergedOptions) => {
       if (response && response.statusCode === 409 && response.body) {
-        console.log('Retry');
         const updatedOptions = {
           headers: {
             'X-Transmission-Session-Id': response.headers['X-Transmission-Session-Id'.toLowerCase()]
           }
         };
-        client.defaults.options = got.mergeOptions(client.defaults.options, updatedOptions);
+        client = client.extend(updatedOptions);
         return retryWithMergedOptions(updatedOptions);
       }
       return response;
@@ -23,10 +22,10 @@ const client = got.extend({
 function formatSize(size, denominator = 1024) {
   if (size < denominator) return `${size} B`;
   else if (size < Math.pow(denominator, 2)) return `${size / denominator} KB`;
-  else if (size < Math.pow(denominator, 3)) return `${size / Math.pow(denominator, 2)} MB`;
-  else if (size < Math.pow(denominator, 4)) return `${size / Math.pow(denominator, 3)} GB`;
-  else if (size < Math.pow(denominator, 5)) return `${size / Math.pow(denominator, 4)} TB`;
-  else if (size < Math.pow(denominator, 6)) return `${size / Math.pow(denominator, 5)} PB`;
+  else if (size < Math.pow(denominator, 3)) return `${(size / Math.pow(denominator, 2)).toFixed(2)} MB`;
+  else if (size < Math.pow(denominator, 4)) return `${(size / Math.pow(denominator, 3)).toFixed(2)} GB`;
+  else if (size < Math.pow(denominator, 5)) return `${(size / Math.pow(denominator, 4)).toFixed(2)} TB`;
+  else if (size < Math.pow(denominator, 6)) return `${(size / Math.pow(denominator, 5)).toFixed(2)} PB`;
 }
 
 function formatTime(time) {
@@ -47,12 +46,11 @@ function formatTime(time) {
 
 (async () => {
   try {
-    console.log('start');
     let timeout = false, finished = false;
-    setTimeout(() => timeout = true, 5.5 * 60 * 60 * 1000);
-    while (timeout || finished) {
+    const timeoutFlag = setTimeout(() => timeout = true, 5.5 * 60 * 60 * 1000);
+    while (!timeout && !finished) {
       console.log(Date.now());
-      const { body } = await client.post('http://localhost:9091/transmission/rpc', {
+      const { body } = await client.post('http://172.22.142.63:9091/transmission/rpc', {
         json: {
           method: 'torrent-get',
           arguments: {
@@ -68,17 +66,17 @@ function formatTime(time) {
           }
         }
       });
-      console.log(body);
       const torrent = body.arguments.torrents[0];
       if (!torrent) {
         finished = true;
-        continue;
+        break;
       }
       const { eta, rateDownload, percentDone, leftUntilDone, sizeWhenDone } = torrent;
       console.log(`${formatSize(sizeWhenDone - leftUntilDone)} / ${formatSize(sizeWhenDone)} (${percentDone * 100}%)  Speed: ${formatSize(rateDownload, 1000)}/s  Remaining: ${formatTime(eta)}`);
       if (percentDone === 1) finished = true;
-      await new Promise(res => setTimeout(() => res(), 30 * 1000));
+      await new Promise(res => setTimeout(() => res(), 5 * 1000));
     }
+    clearTimeout(timeoutFlag);
   }
   catch (error) {
     console.log(error);
