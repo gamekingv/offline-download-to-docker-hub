e7c5e19cf31199bd18de571da1db7a70cde7592a
gamekingv <31853855+gamekingv@users.noreply.github.com>
2021-05-29 09:17:34 +0800

    Update remove.transmission.js

--- remove.transmission.js
+++ remove.transmission.js
@@ -42,46 +42,6 @@ function mapDirectory(root) {
 
 (async () => {
   try {
-    // const list = `${await fsp.readFile('list.txt')}`;
-    // if (!list.includes('  select-file=')) return;
-    // const selected = list.match(/  select-file=(.*)/)[1].split(',').map(index => Number(index));
-    // if (!selected) throw '读取选中文件失败';
-    // const { body: taskInfo } = await client.post('http://localhost:9091/transmission/rpc', {
-    //   json: {
-    //     method: 'torrent-get',
-    //     arguments: {
-    //       fields: [
-    //         'files',
-    //       ],
-    //       ids: [Number(id)]
-    //     }
-    //   }
-    // });
-    // if (!taskInfo) throw '获取种子信息失败';
-    // const files = [];
-    // const { files: torrentFiles } = taskInfo.arguments.torrents[0];
-    // if (torrentFiles) {
-    //   files.push(...torrentFiles.map(({ name, length }, index) => ({ index, name, size: length })).filter(item => !/_____padding_file_\d+_/.test(item.name)));
-    // }
-    // else throw '无法解析种子';
-    // const selectedFiles = files.filter(file => selected.some(index => index === file.index));
-    // const downloadedFiles = mapDirectory('Offline');
-    // const removeFiles = downloadedFiles.filter(file => !selectedFiles.some(selectedFile => `Offline/${selectedFile.name}` === file));
-    // if (removeFiles.length > 0) {
-    //   console.log('清理多余文件：');
-    //   for (const file of removeFiles) {
-    //     console.log(file);
-    //     await fsp.unlink(file);
-    //   }
-    // }
-    // await client.post('http://localhost:9091/transmission/rpc', {
-    //   json: {
-    //     method: 'torrent-stop',
-    //     arguments: {
-    //       ids: [Number(id)]
-    //     },
-    //   }
-    // });
     const selectedFiles = JSON.parse(await fsp.readFile('download-files.json'));
     const downloadedFiles = mapDirectory('Offline');
     const removeFiles = downloadedFiles.filter(file => !selectedFiles.some(selectedFile => `Offline/${selectedFile.name}` === file));
