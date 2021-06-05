9778d76e1c499a4fc78937a9f315590925704fc9
gamekingv <793470218@qq.com>
2021-06-05 11:54:22 +0800

    Initialize

--- Initialize-transmission.js
+++ Initialize-transmission.js
@@ -5,7 +5,8 @@ const fs = require('fs').promises;
     const settings = JSON.parse(await fs.readFile('/etc/transmission-daemon/settings.json'));
     Object.assign(settings, {
       'download-dir': `${__dirname}/Offline`,
-      'rpc-authentication-required': 0
+      'rpc-authentication-required': 0,
+      'umask': 0
     });
     await fs.writeFile('/etc/transmission-daemon/settings.json', JSON.stringify(settings, null, 2));
   }
